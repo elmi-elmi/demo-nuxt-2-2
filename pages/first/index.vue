7d8f6173d5febe781a918e20fa3fd24e216680dd
elmi-elmi <shahrokhelmi@gmail.com>
2021-10-15 21:53:57 +0330

    first commit

---
+++ pages/first/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type="text" placeholder="your route" v-model="userId" />
+    <button @click="onLoadUser">Request</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onLoadUser() {
+      this.$router.push("/first/" + this.userId);
+    }
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <nuxt-link to="first">first</nuxt-link>
+</template>
+
+<script>
+export default {};
+</script>
